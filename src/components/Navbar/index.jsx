3951382092e66df1042ef7c0ba52a84cfc1fd2ec
Raihan Putro Maulana Rizky <raihanworks461@gmail.com>
2024-01-26 15:08:44 +0700

    update: adding new menu item in navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -86,8 +86,11 @@ const Navbar = ({ title, locale, theme, login }) => {
         open={Boolean(anchorElUser)}
         onClose={handleCloseUserMenu}
       >
-          <MenuItem onClick={() => {handleCloseUserMenu, navigate('/profile')}}>
+        <MenuItem onClick={() => {handleCloseUserMenu, navigate('/profile')}}>
             <Typography textAlign="center">Profile</Typography>
+          </MenuItem>
+          <MenuItem onClick={() => {handleCloseUserMenu, navigate('/createnew')}}>
+            <Typography textAlign="center">Create Post</Typography>
           </MenuItem>
           <MenuItem onClick={() => { handleCloseUserMenu, dispatch(setLogin(false)), dispatch(setInfoLoginUser(null))  }}>
             <Typography textAlign="center">Logout</Typography>
